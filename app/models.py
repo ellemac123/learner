a00cbd096642ca731b8e563732351c92f485dec7
Laura Macaluso <lauramacaluso@Lauras-MBP.fios-router.home>
2017-11-25 17:19:26 -0500

    messing with models

--- app/models.py
+++ app/models.py
@@ -19,19 +19,47 @@ YEAR_IN_SCHOOL_CHOICES = (
 )
 
 
-class Teacher(models.Model):
-    first_name = models.CharField("First Name", max_length=25)
-    last_name = models.CharField("Last Name", max_length=50)
-
-
-class Course(models.Model):
-    course_name = models.CharField(max_length=50)
-    instructor = models.OneToOneField(Teacher, related_name="courses")
-
-
 class Student(models.Model):
     # student_id = models.CharField(max_length=20, primary_key=True)
     first_name = models.CharField(max_length=25)
     last_name = models.CharField(max_length=50)
 
-    # student_grade =
+    student_grade = models.CharField(choices=YEAR_IN_SCHOOL_CHOICES, max_length=1)
+
+    @property
+    def full_name(self):
+        """
+
+        :return: a student's full name
+        """
+
+        return '{} {}'.format(self.first_name, self.last_name)
+
+
+class Course(models.Model):
+    course_name = models.CharField(max_length=50)
+    students = models.ManyToManyField(Student, through='Enrollment')
+    # Many students in each course
+
+    # def add_student_to_course(self):
+    #
+
+
+class Teacher(models.Model):
+    first_name = models.CharField("First Name", max_length=25)
+    last_name = models.CharField("Last Name", max_length=50)
+    students = models.ManyToManyField(Student, through='Course')
+
+    @property
+    def full_name(self):
+        """
+
+        :return: a teachers's full name
+        """
+
+        return '{} {}'.format(self.first_name, self.last_name)
+
+
+class Enrollment(models.Model):
+    person = models.ForeignKey(Student, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
